d4c695468b20a8915782e1b4f0dfd1ff0de0850a
mkami3312535546-source <m.kami3312535546@gmail.com>
2025-12-24 04:04:09 +0500

    Add files via upload

--- login.js
+++ login.js
@@ -1,48 +1,31 @@
 const inputUser = document.getElementById('user-name');
 const inputPass = document.getElementById('password');
-const savebtn = document.getElementById('savebtn');
 const login = document.getElementById('login');
-
 
 let users = JSON.parse(localStorage.getItem('users')) || [];
 
-
-savebtn.addEventListener('click', () => {
-    const username = inputUser.value;
-    const password = inputPass.value;  
+login.addEventListener('click', () => {
+    const username = inputUser.value.trim();
+    const password = inputPass.value.trim();
 
     if (!username || !password) {
         alert("Please fill username AND password");
         return;
     }
 
-    
-    const exists = users.some(u => u.username === username);
-    if (exists) {
-        alert("Username already exists!");
-        return;
-    }
+    // Find matching user
+    const user = users.find(
+        u => u.username === username && u.password === password
+    );
 
-    
-    users.push({ username, password });
-    
-    localStorage.setItem('users', JSON.stringify(users));
-    
-    
-    inputUser.value = "";
-    inputPass.value = "";
-
-});
-
-    login.addEventListener('click', () => {
-    const username = inputUser.value.trim();
-    const password = inputPass.value.trim();
-
-    const checkUser = users.some(u => u.username === username && u.password === password);
-
-    if (checkUser) {
+    if (user) {
+        // Login success
+        localStorage.setItem("loggedInUser", username);
         window.location.href = "index.html";
     } else {
         alert("Username or password incorrect");
     }
+
+    inputUser.value = "";
+    inputPass.value = "";
 });
